883361e6eab6df8716a63de23c964a8fc24fabc0
Dreamer-247 <118843512+Dreamer-247@users.noreply.github.com>
2023-04-01 13:11:35 +0600

    Create setup2.sh

---
+++ setup2.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+cd ~
+
+mv -f config.fish /data/data/com.termux/files/usr/etc/fish/config.fish
+echo "" > /data/data/com.termux/files/home/.config/fish/functions/fish_prompt.fish
+mv -f /data/data/com.termux/files/home/TerCool/fish_prompt.fish /data/data/com.termux/files/home/.config/fish/functions/fish_prompt.fish
+
+termux-reload-settings
+clear
