c01503d5762bcc7c1ab190beceb146ace7f2efd3
Dreamer-247 <118843512+Dreamer-247@users.noreply.github.com>
2023-04-01 13:41:57 +0600

    Update setup2.sh

--- setup2.sh
+++ setup2.sh
@@ -7,9 +7,10 @@ mv -f ~/TerCool/config.fish /data/data/com.termux/files/usr/etc/fish/config.fish
 echo "" > ~/.config/fish/functions/fish_prompt.fish
 mv -f ~/TerCool/fish_prompt.fish ~/.config/fish/functions/fish_prompt.fish
 
-cd ~
-
 termux-reload-settings
-clear
 
 rm -rf ~/TerCool/
+
+cd ~
+clear
+exec bash
