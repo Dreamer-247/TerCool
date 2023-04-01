7bfc0afde6275f7cb4d89b14e6d18e8953dbc7d9
Dreamer-247 <118843512+Dreamer-247@users.noreply.github.com>
2023-04-01 13:30:48 +0600

    Update setup2.sh

--- setup2.sh
+++ setup2.sh
@@ -2,9 +2,10 @@
 
 cd ~
 
-mv -f data/data/com.termux/files/home/TerCool/config.fish /data/data/com.termux/files/usr/etc/fish/config.fish
-echo "" > /data/data/com.termux/files/home/.config/fish/functions/fish_prompt.fish
-mv -f /data/data/com.termux/files/home/TerCool/fish_prompt.fish /data/data/com.termux/files/home/.config/fish/functions/fish_prompt.fish
+echo "" > /data/data/com.termux/files/usr/etc/fish/config.fish
+mv -f ~/TerCool/config.fish /data/data/com.termux/files/usr/etc/fish/config.fish
+echo "" > ~/.config/fish/functions/fish_prompt.fish
+mv -f ~/TerCool/fish_prompt.fish ~/.config/fish/functions/fish_prompt.fish
 
 cd ~
 
