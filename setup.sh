a863a6ded6a5432e59a20e3bb3da4189fc9d888c
Dreamer-247 <118843512+Dreamer-247@users.noreply.github.com>
2023-04-01 12:30:47 +0600

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#Installing_fish
+pkg install fish -y
+
+#removing_motd
+cd /data/data/com.termux/files/usr/etc
+rm motd motd-playstore
+
+#Updating_bash.bashrc
+echo -e "fish" >> /data/data/com.termux/files/usr/etc/bash.bashrc
+
+#Applying_changes
+source ~/.bashrc
+
